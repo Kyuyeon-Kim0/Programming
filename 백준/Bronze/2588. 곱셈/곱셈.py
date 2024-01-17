c0de760931f2a5773e122a70a650d9a1fa9658d1
Kyuyeon-Kim0 <126145542+Kyuyeon-Kim0@users.noreply.github.com>
2024-01-17 17:50:42 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,8 @@
+first = int(input())
+second = input()
+
+third = print(first * int(second[2]))
+fourth = print(first * int(second[1]))
+fifth = print(first * int(second[0]))
+
+print(first * int(second))
