017cc6a0944397390758bc930c55306a648c98dc
Kyuyeon-Kim0 <126145542+Kyuyeon-Kim0@users.noreply.github.com>
2024-01-24 18:16:04 +0900

    [Silver I] Title: 절댓값 힙, Time: 140 ms, Memory: 39824 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,14 +1,18 @@
 import sys
 import heapq
+input = sys.stdin.readline
 
 n = int(input())
 heap = []
+
 for i in range(n):
-    x = int(sys.stdin.readline())
+    x = int(input())
     if x == 0:
         if heap:
+            # 절댓값이 가장 작은 값 제거, 반환 (항상 루트에 위치)
             print(heapq.heappop(heap)[1])
         else:
             print(0)
     else:
-        heapq.heappush(heap,(abs(x),x))
+        # 절댓값, 입력값 push
+        heapq.heappush(heap,(abs(x), x))
